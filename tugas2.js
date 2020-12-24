70c18b4efa1048a116fb5046960219613f95b9d6
User-niomic <user@niomic.com>
2020-12-25 03:07:58 +0700

    TUGAS 2 JS
    Tugas ini dikumpulkan untuk Niomic

---
+++ tugas2.js
@@ -0,0 +1,13 @@
+var number=1234;
+var string="Niomic"
+var bolean=true;
+var kosong=null;
+var undefine;
+var array=[1,2,3,4,5]
+
+console.log(number);
+console.log(string);
+console.log(bolean);
+console.log(kosong);
+console.log(undefine);
+console.log(array);
